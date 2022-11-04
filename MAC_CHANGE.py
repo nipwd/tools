99c0f060513b3f06000c8bc73d760d27e48c5bf2
nipwd <67521506+nipwd@users.noreply.github.com>
2022-11-03 23:42:11 -0300

    Create MAC_CHANGE.py

---
+++ MAC_CHANGE.py
@@ -0,0 +1,41 @@
+import subprocess
+import optparse
+import re
+def get_arguments():
+    parser= optparse.OptionParser()
+    parser.add_option("-i" , "--interface", dest="interface", help="interface para cambiar MAC")
+    parser.add_option("-m" , "--mac", dest="new_mac", help="nueva direccion MAC")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Por favor indincar una interfaz, --help para ayuda")
+    elif not options.new_mac:
+        parser.error("[-] Por favor indincar una direccion MAC, --help para ayuda")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] cambiando Mac para " + interface + "a " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    # ejemplo:  sudo python mac_change.py --interface eth0 --mac 00:11:22:33:44:54
+
+def get_current_mac(interface):
+    ifconfig_results = subprocess.check_output(["ifconfig", options.interface])
+    mac_addres_search_ressult = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_results))
+    if mac_addres_search_ressult:
+        return(mac_addres_search_ressult.group(0))
+    else:
+        print("[-] No funciono")
+
+(options) = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("current mac = " + str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+
+current_mac= get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC nueva" , current_mac)
+else:
+    print("[-] ERROR EN CAMBIO")
